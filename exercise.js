c8f9cffd9e9311661edc5423f9edaab5a0fb0d46
Sayidwebdeveloper <sayidfuad4027@gmail.com>
2023-01-03 22:34:56 +0300

    My Homework

--- exercise.js
+++ exercise.js
@@ -1,2 +1,63 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+
+// Question  Number one
+
+
+function describeCountry(city, population, country) {
+    console.log(`The capital city is ${city} , the population is ${population} And the country is ${country} `)
+
+}
+ describeCountry( "hargeisa", 3.5 , "somaliland")
+
+
+
+ //Question Number two 
+
+function countries(country){
+    console.log(country)
+
+}
+countries("uganda ")
+countries("USA")
+countries("canada")
+
+
+//Qeustion Number Three
+
+function percentageOfworld(){
+
+}
+
+
+
+
+
+
+
+//PART TWO ARRAYS QUESTION
+// Question Number five
+
+let population = ["somaliland","Somalia", "Kenya", "D.jabouti"]
+let Neighbours = ["somalia","d.jabouti","kenya","Ethopia"]
+console.log(population, Neighbours)
+
+// PART FOR LOOP QUESTION
+
+for(let i = 1; i<=4; i++ ){
+console.log(i)
+}
+
+for( let i = 4; i >=1; i--){
+    console.log(i)
+}
+
+//PART FOR WHILE LOOP QUESTION
+
+let i =1 
+while( i <=4){
+    console.log(i)
+    i++;
+
+}
+
